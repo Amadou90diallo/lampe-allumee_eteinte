a4b4f9587480d6b721a32e7ca3503e2013cbd2b9
Amadou DIALLO <alpha0amadou00@gmail.com>
2021-10-26 18:16:02 +0200

    The first commit

---
+++ javaScript/samaJS.js
@@ -0,0 +1,8 @@
+function allumee_lampe(){
+	document.getElementById('lampe').src = "photos/lampe_allumee.jpg";
+}
+
+
+function eteindre_lampe(){
+	document.getElementById('lampe').src = "photos/lampe_eteinte.jpg";
+}
